e4ad3ae5644b0dce9b28a3df18863ec4161d1f73
ggjorven <Jorbenvanderwal@gmail.com>
2024-05-13 20:10:46 +0200

    Changed how Binary is written and read + made it customizable.

--- Core/src/Flow/Binary/Binary.hpp
+++ Core/src/Flow/Binary/Binary.hpp
@@ -6,6 +6,7 @@
 #include <filesystem>
 
 #include "Flow/Core/Core.hpp"
+#include "Flow/Binary/BinaryOptions.hpp"
 
 namespace Flow::Binary
 {
@@ -18,18 +19,24 @@ namespace Flow::Binary
 
 		// Writing
 		template<typename T>
-		void Write(T data);
-		template<>
-		void Write<std::string>(std::string str);
+		inline void Write(T data)
+		{
+			Convert<T>::Write(m_Write, data);
+		}
 
 		template<typename T>
-		File& operator << (T data);
+		File& operator << (T data)
+		{
+			Write<T>(data);
+			return *this;
+		}
 
 		// Reading
 		template<typename T>
-		T Read(size_t offset = 0);
-		template<>
-		std::string Read<std::string>(size_t offset);
+		inline T Read(size_t offset = 0)
+		{
+			return Convert<T>::Read(m_Read, offset);
+		}
 
 	private:
 		void StartRead();
@@ -45,65 +52,5 @@ namespace Flow::Binary
 		std::vector<char> m_Read = { };
 		std::ofstream m_Write = {};
 	};
-
-	// Writing
-	template<typename T>
-	inline void File::Write(T data)
-	{
-		m_Write.write(reinterpret_cast<const char*>(&data), sizeof(T));
-	}
-
-	template<>
-	inline void File::Write(std::string str)
-	{
-		int32_t size = (int32_t)str.size();
-		m_Write.write(reinterpret_cast<const char*>(&size), sizeof(int32_t));
-
-		m_Write.write(str.c_str(), size);
-	}
-
-	template<typename T>
-	inline File& File::operator << (T data)
-	{
-		Write<T>(data);
-		return *this;
-	}
-
-	// Reading
-	template<typename T>
-	inline T File::Read(size_t offset)
-	{
-		// TODO: Logging
-		T def = {};
-		memcpy(&def, &m_Read[offset], sizeof(T));
-		offset += sizeof(T);
-
-		return def;
-	}
-
-	template<>
-	inline std::string File::Read<std::string>(size_t offset)
-	{
-		if (offset + sizeof(int32_t) > m_Read.size())
-		{
-			// TODO: Logging
-			return {};
-		}
-
-		int32_t size = 0;
-		memcpy(&size, &m_Read[offset], sizeof(int32_t));
-		offset += sizeof(int32_t);
-
-		if (offset + size > m_Read.size())
-		{
-			// TODO: Logging
-			return {};
-		}
-
-		std::string data(m_Read.begin() + offset, m_Read.begin() + offset + size);
-		offset += size; // Unnecessary
-
-		return data;
-	}
 
 }
--- Core/src/Flow/Binary/BinaryOptions.hpp
+++ Core/src/Flow/Binary/BinaryOptions.hpp
@@ -1,7 +1,61 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include <fstream>
+
 namespace Flow::Binary
 {
+
+	template<typename T>
+	struct Convert
+	{
+	public:
+		static void Write(std::ofstream& output, T data)
+		{
+			output.write(reinterpret_cast<const char*>(&data), sizeof(T));
+		}
+
+		static T Read(std::vector<char>& input, size_t offset = 0)
+		{
+			T result = {};
+			memcpy(&result, &input[offset], sizeof(T));
+
+			return result;
+		}
+	};
+
+	template<>
+	struct Convert<std::string>
+	{
+	public:
+		static void Write(std::ofstream& output, std::string data)
+		{
+			int32_t size = (int32_t)data.size();
+			output.write(reinterpret_cast<const char*>(&size), sizeof(int32_t));
+
+			output.write(data.c_str(), size);
+		}
+
+		static std::string Read(std::vector<char>& input, size_t offset = 0)
+		{
+			if (offset + sizeof(int32_t) > input.size())
+			{
+				return {};
+			}
+
+			int32_t size = 0;
+			memcpy(&size, &input[offset], sizeof(int32_t));
+			offset += sizeof(int32_t);
+
+			if (offset + size > input.size())
+			{
+				return {};
+			}
+
+			return std::string(input.begin() + offset, input.begin() + offset + size);
+		}
+	};
 
 	// Used for getting the size of an object in binary files
 	template<typename T>
